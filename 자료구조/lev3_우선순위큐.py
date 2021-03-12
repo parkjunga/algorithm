6a54eadfc4b40f45514970da49a613b3bdff7797
parkjungah <asdfg98712@gmail.com>
2021-03-12 22:27:19 +0900

    heap 자료구조 다시 풀이

---
+++ 자료구조/lev3_우선순위큐.py
@@ -0,0 +1,9 @@
+import heapq as hq
+def solution(operations):
+    answer = []
+
+    return answer
+
+operations = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
+ans = solution(operations)
+print(ans)
--- 자료구조/lev_heap.py
+++ 자료구조/lev_heap.py
@@ -1,14 +1,18 @@
-import heapq
+import heapq as hq
 def solution(scoville, K):
     answer = 0
-
-    while K >= min(scoville):
-        scoville.sort()
-        tmp = scoville[0] + (scoville[1] * 2 )
-        scoville = scoville[2:]
-        scoville.insert(0,tmp)
-        answer += 1
-    #print(scoville)
+    hq.heapify(scoville) # 힙구조로 바뀜
+    #print(hq.heappop(scoville)) # 최소값 뽑고
+    while True: # while 문을 돌리면서
+        first = hq.heappop(scoville)
+        if K > first:
+            tmp = first + hq.heappop(scoville) * 2
+            hq.heappush(scoville,tmp)
+            answer += 1
+        else:
+            break
+        if len(scoville) == 0:
+            return -1
     return answer
 
 
