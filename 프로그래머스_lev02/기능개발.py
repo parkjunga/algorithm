98a503274ded50888555aa3fe38bf1977c6cbe5d
parkjungah <asdfg98712@gmail.com>
2021-06-02 22:09:58 +0900

    시도는 했지만...

---
+++ 프로그래머스_lev02/기능개발.py
@@ -0,0 +1,8 @@
+import math
+def solution(progresses, speeds):
+    answer = []
+    return answer
+
+progresses = [95, 90, 99, 99, 80, 99]
+speeds = [1, 1, 1, 1, 1, 1]
+print(solution(progresses,speeds))
