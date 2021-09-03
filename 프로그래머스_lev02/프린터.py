a75198ab2cf2f53f6fb3f1e5a7cda1005f637c93
박정아 <djdjejh@naver.com>
2021-09-04 08:40:26 +0900

    UPDATE 스택문제 시도
    1차시도

--- 프로그래머스_lev02/프린터.py
+++ 프로그래머스_lev02/프린터.py
@@ -2,10 +2,24 @@ def solution(priorities, location):
     answer = 0
     new = list(enumerate(priorities))
     # 거꾸로 + 순서
-    new.sort(key=lambda x:(x[1],x[0]),reverse=True)
-    print(new)
-priorities = [1, 1, 9, 1, 1, 1] # 중요도 순서
-location = 0# 내가 인쇄 요청한 문서 몇 번쟤
+    test = sorted(new,key=lambda x: (-x[1]))
+    #print(test[0][0])
+    stack = []
+    stack.append(test[0][0])
+    for i in range(len(test)):
+        if stack[0] < test[i][0]:
+            stack.append(test[i][0])
+    for j in range(len(test)):
+        if test[j][0] not in stack:
+            stack.append(test[j][0])
+
+    for t in range(len(stack)):
+        if stack[t] == location:
+            answer = t+1
+    return answer
+
+priorities = [1,1,9,1,1,1] # 중요도 순서
+location = 0 # 내가 인쇄 요청한 문서 몇 번쟤
 
 solution(priorities,location)
 
